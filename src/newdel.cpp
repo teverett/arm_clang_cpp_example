e830700f6b998bdb3b270b0d3f3f1cb14205c145
Tom Everett <tom@khubla.com>
2017-03-18 15:37:45 -0600

    a very dumb new

--- src/newdel.cpp
+++ src/newdel.cpp
@@ -4,8 +4,11 @@
 * custom new and delete operators needed by C++
 */
 
-void* operator new  ( unsigned int count ){
+extern void* heap_top;
 
+void* operator new  (unsigned int count){
+	// return the whole heap.  i am not a smart man, but i know that my program only has 1 "new"
+	return heap_top;
 }
 
 void operator delete (void* ptr){
