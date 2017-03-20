3411d72258375d09bf07afbb8e31d911388b14fe
Tom Everett <tom@khubla.com>
2017-03-19 19:04:19 -0600

    heap games

--- src/newdel.cpp
+++ src/newdel.cpp
@@ -1,30 +1,35 @@
 
 
 extern "C" {
-#include "image.h"
-#include "serial.h"
+	#include "image.h"
+	#include "serial.h"
 }
 
 /*
 * custom new and delete operators needed by C++
 */
-
+/*
+* Behold! The worst imaginable Heap Implementation!
+*/
 int c=0;
-static int blocksize=1000;
+static int blocksize=100;
 
 void* operator new  (unsigned int count){
-
-	unsigned int t = (unsigned int) __heap_top;
-	unsigned int block = t+(c*blocksize);
+	unsigned int t = (unsigned int) &__heap_top;
+	unsigned int* block = (unsigned int*) (t+(c*blocksize));
 	c=c+1;
 
-//	print_uart0_str("new: ");
-//	print_uart0_int((unsigned int) __heap_top);
-//	print_uart0_str("\n");
+	print_uart0_str("new: ");
+	print_uart0_int((unsigned int) block);
+	print_uart0_str(" size: ");
+	print_uart0_int((unsigned int) count);
+	print_uart0_str("\n");
 
 	return (void*) block;
 }
 
 void operator delete (void* ptr){
-	print_uart0_str("delete\n");
+	print_uart0_str("delete: ");
+	print_uart0_int((unsigned int) ptr);
+	print_uart0_str("\n");
 }
