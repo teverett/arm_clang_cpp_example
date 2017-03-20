ec4b5ed363695a62d759bf005e4541be4a72e599
Tom Everett <tom@khubla.com>
2017-03-19 21:38:44 -0600

    whups

--- src/obj1.cpp
+++ src/obj1.cpp
@@ -5,15 +5,15 @@ extern "C" {
 }
 
 Obj1::Obj1(int x) {
-	print_uart0_str("Obj1 int ctor with value: ");
-	print_uart0_int(x);
-	print_uart0_str("\n");
 	this->x = x;
+	print_uart0_str("Obj1 int ctor with value: ");
+	print_uart0_int(this->x);
+	print_uart0_str("\n");
 }
 
 Obj1::Obj1() {
 	print_uart0_str("Obj1 default ctor\n");
-	x = 77;
+	x = 33;
 }
 
 Obj1::~Obj1() {
