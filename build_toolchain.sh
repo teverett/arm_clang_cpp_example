3d32496ebfd4c4972a04299f5345d269720d0feb
Tom Everett <tom@khubla.com>
2020-06-07 17:37:41 -0600

    ff

--- build_toolchain.sh
+++ build_toolchain.sh
@@ -5,7 +5,7 @@ set -e
 
 if [ $# -eq 0 ]; then
 #    TARGETS=( arm-none-eabi arm-none-elf mips-none-elf mipsel-none-elf mips64-none-elf mips64el-none-elf sparc-none-elf avr-none-elf)
-    TARGETS=(arm-none-eabi)
+    TARGETS=arm-none-eabi
 else
     TARGETS=$1
 fi
