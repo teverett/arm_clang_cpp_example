5327ae1a9bcabdedf993dad885498bacaed55290
Tom Everett <tom@khubla.com>
2020-06-07 17:43:37 -0600

    bash on linux

--- build_toolchain.sh
+++ build_toolchain.sh
@@ -2,13 +2,14 @@
 
 # exit on error
 set -e
+
 # TARGETS=( arm-none-eabi arm-none-elf mips-none-elf mipsel-none-elf mips64-none-elf mips64el-none-elf sparc-none-elf avr-none-elf)
 
-if [ $# -eq 0 ]; then
-    TARGETS=("arm-none-eabi" "arm-none-elf")
-else
+#if [ $# -eq 0 ]; then
+#    TARGETS=("arm-none-eabi" "arm-none-elf")
+#else
     TARGETS=$1
-fi
+#fi
 
 printf "binutils targets are: ";
 for i in "${TARGETS[@]}" 
